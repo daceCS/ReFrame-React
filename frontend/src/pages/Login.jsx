95d3c0f8877e950ef063810cbf1ed9d93f055c00
Thai2229 <95368279+Thai2229@users.noreply.github.com>
2023-05-18 13:44:56 -0700

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,33 @@
+
+
 function Login() {
-  return <h1>Login up page</h1>;
+  return <div class="container">
+  <div class="screen">
+      <div class="screen__content">
+          <div class="login">
+              <div class="login__field">
+                  <i class="login__icon fas fa-user"></i>
+                  <input type="text" class="login__input" id="username" placeholder="Enter Username" onkeypress="return event.charCode != 32" maxlength="14"/>
+              </div>
+              <div class="login__field">
+                  <i class="login__icon fas fa-lock"></i>
+                  <input type="password" class="login__input" id="password" placeholder="Enter Password" onkeypress="return event.charCode != 32"/>
+              </div>
+              <button class="create-account-button" role="button" id="create-account-button" onclick="login()">Login</button>
+              <br/>
+              <br/>
+              <a href="/create-account" id="signup-link">Dont have an account? Sign Up</a>
+          </div>
+      </div>
+
+      <div class="screen__background">
+          <span class="screen__background__shape screen__background__shape4"></span>
+          <span class="screen__background__shape screen__background__shape3"></span>
+          <span class="screen__background__shape screen__background__shape2"></span>
+          <span class="screen__background__shape screen__background__shape1"></span>
+      </div>
+  </div>
+</div>;
 }
 
 export default Login;
