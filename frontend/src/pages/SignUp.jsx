7bef894c00ab257ebee95fee5b85ff580d97db53
Thai2229 <95368279+Thai2229@users.noreply.github.com>
2023-05-18 13:45:14 -0700

    Update SignUp.jsx

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,5 +1,30 @@
+import "../css/signup.css";
+
 function SignUp() {
-  return <h1>sign up page</h1>;
+  return <div className="container">
+  <div className="screen">
+      <div className="screen__content">
+          <div className="login">
+              <div className="login__field">
+                  <i className="login__icon fas fa-user"></i>
+                  <input type="text" className="login__input" id="username" placeholder="Enter Username" maxlength="14" onkeypress="return event.charCode != 32" />
+              </div>
+              <div className="login__field">
+                  <i className="login__icon fas fa-lock"></i>
+                  <input type="password" className="login__input" id="password" placeholder="Enter Password" onkeypress="return event.charCode != 32"/>
+              </div>
+              <button className="create-account-button" role="button" id="create-account-button" onclick="createAccount()">Create account</button>
+          </div>
+      </div>
+      <div className="screen__background">
+          <span className="screen__background__shape screen__background__shape4"></span>
+          <span className="screen__background__shape screen__background__shape3"></span>
+          <span className="screen__background__shape screen__background__shape2"></span>
+          <span className="screen__background__shape screen__background__shape1"></span>
+      </div>
+  </div>
+</div>;
+  
 }
 
 export default SignUp;
