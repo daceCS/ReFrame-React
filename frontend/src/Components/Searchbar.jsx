c615a4f6520501354235531bd9fd2bcd2400117b
louis-129 <128095517+louis-129@users.noreply.github.com>
2023-05-19 13:42:39 -0700

    Add files via upload

---
+++ frontend/src/Components/Searchbar.jsx
@@ -0,0 +1,22 @@
+function SearchBar(){
+    return(
+    <>
+    
+    <form action="" className="search-bar">
+    <input type="text" id="search" placeholder="Search Users" name="SearchStuff" autocomplete="off" oninput="searchBar()"/>
+    <button type="submit" id="searchbutton"><img src = "../images/search.png"/></button>
+    </form>
+    <div className="dropdown">
+        <ul id="dropdown-list">
+
+        </ul>
+    </div>
+    
+    </>
+    
+
+)
+        
+    
+}
+export default SearchBar;
---
+++ frontend/src/Components/Sorter.jsx
@@ -0,0 +1,12 @@
+function Sorter(){
+    return(<div className="content-options">
+    <p id="sort-title">Sort By:</p>
+    <input type="button" id="sort-top-voted" value="Most Liked" onclick="sortLiked()"/>
+    <input type="button" id="sort-following" value="Following" onclick="sortFollowing()"/>
+    <input type="button" id="sort-new" value=" Sort New" onclick="sortNew()"/>
+    <input type="button" id="sort-old" value="Sort Oldest" onclick="sortOld()"/>
+</div>)
+        
+    
+}
+export default Sorter;
