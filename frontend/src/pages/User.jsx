962b0e4cf6bdf838e2085942adef31647bca6612
louis-129 <128095517+louis-129@users.noreply.github.com>
2023-05-23 12:31:15 -0700

    Update User.jsx

--- frontend/src/pages/User.jsx
+++ frontend/src/pages/User.jsx
@@ -1,5 +1,8 @@
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
+import SearchBar from "../Components/Searchbar";
+import ContentOptions from "../Components/ContentOptions";
+import UserInfo from "../Components/UserInfo";
 
 function User() {
   const { user } = useParams();
@@ -14,7 +17,28 @@ function User() {
   return (
     <>
       <title>{user}</title>
-      <h1>User Page for {user}</h1>
+      <body>
+      <header>
+          <nav>
+              <input type="button" value="Sign Out" id="sign-out" onclick="signOut()"/>
+              <SearchBar/>
+          </nav>
+      </header>
+      <aside>
+          <div class="menu-div">
+              <nav class="menu">
+                  <a href="/feed" id="home-link">Home</a>
+                  <a href="/createPost" id="new-post-link">Create Post</a>
+              </nav>
+          </div>
+      </aside>
+
+      <ContentOptions/>
+
+      <UserInfo/>
+
+  </body>
+
     </>
   );
 }
