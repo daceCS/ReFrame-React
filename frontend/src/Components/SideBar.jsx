a55b29b1dd7802b17b4695fab06f4ff85c9efe77
Thai2229 <95368279+Thai2229@users.noreply.github.com>
2023-05-24 13:34:46 -0700

    Update SideBar.jsx

--- frontend/src/Components/SideBar.jsx
+++ frontend/src/Components/SideBar.jsx
@@ -3,12 +3,44 @@ function SideBar() {
     <aside>
       <div className="menu-div">
         <nav className="menu">
-          <a href="/feed" id="home-link">
-            Home
-          </a>
-          <a href="/createPost" id="new-post-link">
-            Create Post
-          </a>
+
+        <a href="/feed" class="button2 button--piyo" id = "home-link">
+        <div class="button__wrapper">
+            <span class="button__text">Home</span>
+        </div>
+        <div class="characterBox">
+            <div class="character wakeup">
+                <div class="character__face"></div>
+            </div>
+            <div class="character wakeup">
+                <div class="character__face"></div>
+            </div>
+            <div class="character">
+                <div class="character__face"></div>
+            </div>
+        </div>
+    </a>
+
+          <a href="/create" class="button button--pen" id = "create-post-link">
+        <div class="button__wrapper">
+            <span class="button__text">Create</span>
+        </div>
+        <div class="characterBox">
+            <div class="character wakeup">
+                <div class="character__face"></div>
+                <div class="charactor__face2"></div>
+            </div>
+            <div class="character wakeup">
+                <div class="character__face"></div>
+                <div class="charactor__face2"></div>
+            </div>
+            <div class="character">
+                <div class="character__face"></div>
+                <div class="charactor__face2"></div>
+            </div>
+        </div>
+    </a>
+
         </nav>
       </div>
     </aside>
