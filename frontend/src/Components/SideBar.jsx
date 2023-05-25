0be81077b7ba6f1ff286eef41c14e2ffd6335360
louis-129 <128095517+louis-129@users.noreply.github.com>
2023-05-25 12:33:18 -0700

    Update SideBar.jsx

--- frontend/src/Components/SideBar.jsx
+++ frontend/src/Components/SideBar.jsx
@@ -1,46 +1,15 @@
+import "../css/feed.css";
 function SideBar() {
   return (
     <aside>
       <div className="menu-div">
-        <nav className="menu">
-
-        <a href="/feed" class="button2 button--piyo" id = "home-link">
-        <div class="button__wrapper">
-            <span class="button__text">Home</span>
-        </div>
-        <div class="characterBox">
-            <div class="character wakeup">
-                <div class="character__face"></div>
-            </div>
-            <div class="character wakeup">
-                <div class="character__face"></div>
-            </div>
-            <div class="character">
-                <div class="character__face"></div>
-            </div>
-        </div>
-    </a>
-
-          <a href="/create" class="button button--pen" id = "create-post-link">
-        <div class="button__wrapper">
-            <span class="button__text">Create</span>
-        </div>
-        <div class="characterBox">
-            <div class="character wakeup">
-                <div class="character__face"></div>
-                <div class="charactor__face2"></div>
-            </div>
-            <div class="character wakeup">
-                <div class="character__face"></div>
-                <div class="charactor__face2"></div>
-            </div>
-            <div class="character">
-                <div class="character__face"></div>
-                <div class="charactor__face2"></div>
-            </div>
-        </div>
-    </a>
-
+        <nav >
+          <a href="/feed" id="home-link" className="menu">
+            Home
+          </a>
+          <a href="/createPost" id="new-post-link" className="createButton">
+            Create Post
+          </a>
         </nav>
       </div>
     </aside>
