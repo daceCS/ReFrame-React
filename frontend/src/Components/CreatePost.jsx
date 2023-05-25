74cd762dfe5adfc1041f5a491da6d07f4ed38f48
Thai2229 <95368279+Thai2229@users.noreply.github.com>
2023-05-25 13:18:41 -0700

    Update CreatePost.jsx

--- frontend/src/Components/CreatePost.jsx
+++ frontend/src/Components/CreatePost.jsx
@@ -3,7 +3,23 @@
 function CreatePost() {
     return (
       <>
-        <div id="create-post-interaction">
+        <div id="create-post-interaction" className = "button button--pen">
+        <div class="button__wrapper">
+        </div>
+        <div class="characterBox">
+            <div class="character wakeup">
+                <div class="character__face"></div>
+                <div class="charactor__face2"></div>
+            </div>
+            <div class="character wakeup">
+                <div class="character__face"></div>
+                <div class="charactor__face2"></div>
+            </div>
+            <div class="character">
+                <div class="character__face"></div>
+                <div class="charactor__face2"></div>
+            </div>
+        </div>
         <input
           type="button"
           id="create-post-interact-field"
