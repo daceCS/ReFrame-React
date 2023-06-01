9120b64808d2c4129fcb8e20915ee938cec12329
Thai2229 <95368279+Thai2229@users.noreply.github.com>
2023-05-31 18:21:39 -0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,18 @@
 import { useState } from "react";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SignUp from "./pages/SignUp";
-import Login from "./pages/Login";
 import Home from "./pages/Feed";
-import User from "./pages/User";
 import ExamplePost from "./pages/ExamplePost";
-import ExampleUpload from "./pages/ExampleUpload";
+import Upload from "./pages/ExampleUpload";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
-        <Route path="/users/:user" element={<User />} />
         <Route path="*" element={<h1>No Page Found</h1>} />
         <Route path="/example" element={<ExamplePost />} />
-        <Route path="/upload" element={<ExampleUpload />} />
+        <Route path="/upload" element={<Upload />} />
       </Routes>
     </>
   );
